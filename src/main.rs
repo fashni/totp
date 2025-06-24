edc1ccac549f73176c76ba9b21d0b22221197f6e
fashni <fashni@tuta.io>
2025-06-24 11:59:25 +0700

    add option to input the secret from stdin, add error handling in decoding secret

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::io::{self, Write};
 use std::time::{SystemTime, UNIX_EPOCH};
 use std::env;
 use totp_lite::{totp_custom, Sha1, DEFAULT_STEP};
@@ -7,10 +8,7 @@ fn main() {
   let mut args = env::args().skip(1);
   let secret = match args.next() {
     Some(arg) => arg,
-    None => {
-      println!("No secret provided");
-      return
-    }
+    None => input("Enter the secret")
   };
 
   let length = secret.len();
@@ -19,8 +17,26 @@ fn main() {
     return
   }
 
-  let decoded_secret = base32::decode(Rfc4648 { padding: false }, &secret.to_uppercase()).unwrap();
+  let decoded_secret = match base32::decode (
+    Rfc4648 { padding: false },
+    &secret.to_uppercase()
+  ) {
+    Some(decoded) => decoded,
+    None => {
+      println!("Failed to decode secret");
+      return
+    }
+  };
+
   let seconds: u64 = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
   let result = totp_custom::<Sha1>(DEFAULT_STEP, 6, &decoded_secret, seconds);
   println!("{}", result);
+}
+
+fn input(prompt: &str) -> String {
+  print!("{}: ", prompt);
+  io::stdout().flush().unwrap();
+  let mut buf = String::new();
+  io::stdin().read_line(&mut buf).expect("Failed to read input");
+  buf.trim().to_string()
 }
